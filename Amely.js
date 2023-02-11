4272fe754b4428a905a38739302e40bc604296ca
LandLena2 <e.badmazhapova@mail.ru>
2023-02-11 20:55:30 +0300

    в один файл

--- Amely.js
+++
@@ -1,6 +0,0 @@
-const NAME = 'Елена';
-const AGE = '25';
-const TOWN = 'Улан-Удэ';
-let yearOfBirth = 1998;
-
-console.log(`Я ${NAME}, мой возраст ${2023-yearOfBirth} лет. Я родилась в г. ${TOWN}, респ. Бурятия`);
