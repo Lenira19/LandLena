96400ff383fa316cf8c123b62bb33999f244f52d
LandLena2 <e.badmazhapova@mail.ru>
2023-02-21 17:27:35 +0300

    Словари и списки JS

--- JS_2/kitchen.js
+++ JS_2/kitchen.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-
 let kitchen= {
     soup:['onion','potato','tomato'],
     tart:['eggs','milk','suger'],
@@ -33,9 +24,7 @@ list_price_kitchen.unshift(price);
 
 
 
-    </script>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+
+
