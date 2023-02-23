8181274d98099fdce64fad7924d42f5dcd7c8cd0
LandLena2 <e.badmazhapova@mail.ru>
2023-02-23 21:57:19 +0300

    question

--- JS_3/alarm.js
+++ JS_3/alarm.js
@@ -1,10 +1,10 @@
-let question = prompt('What is year launch first person in space?');
+let question = prompt('What was year launch first person in space?');
 if (question==1961){
     alert('Correct answer');
 } else if(question!=1961){
     alert('Wrong answer')};
 
-    
+
 
 
 
