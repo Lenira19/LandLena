faf8773d0f4167f43eac94111ec2f3850ee832cb
LandLena2 <e.badmazhapova@mail.ru>
2023-02-25 20:03:20 +0300

    4 задание

--- JS_2/kitchen.js
+++ JS_2/kitchen.js
@@ -1,45 +1,19 @@
 let soup_with_noodls= {
-    ingradients: ['potato','salt','noodlse'],
-    price: 290,
-
+    ingradients: ['potato','salt','noodlse']
 };
 
 let fried_eggs = {
-    ingradients:['eggs','onion','vagatable_oil'],
-    price:199
+    ingradients:['eggs','onion','vagatable_oil']
 }; 
 
 let olivie_salat = {
-    ingradients:['peas','eggs','mayonnaise','carrot'],
-    price:499
+    ingradients:['peas','eggs','mayonnaise','carrot']
 };
 
 
-let list_delishes = [soup_with_noodls,fried_eggs,olivie_salat];
-
-list_delishes.pop();
-
-olivie_salat.ingradients.shift();
-
-list_delishes.unshift(olivie_salat);
-
-let price_delishes_pre={
-    soup_with_noodls:soup_with_noodls.price,
-    fried_eggs:fried_eggs.price,
-    olivie_salat:olivie_salat.price
-};
 
 
-let price_delishes_aft={
-    soup_with_noodls:price_delishes_pre.soup_with_noodls*1.5,
-    fried_eggs:price_delishes_pre.fried_eggs*1.5,
-    olivie_salat:price_delishes_pre.olivie_salat*1.5
-};
 
-
-let soup_with_noodls_income = price_delishes_aft.soup_with_noodls - price_delishes_pre.soup_with_noodls;
-let fried_eggs_income = price_delishes_aft.fried_eggs - price_delishes_pre.fried_eggs;
-let olivie_salat_income = price_delishes_aft.olivie_salat - price_delishes_pre.olivie_salat;
 
 console.log('доход с блюда суп лапша = ', soup_with_noodls_income);
 console.log('доход с блюда жаренные яйца = ', fried_eggs_income);
---
+++ JS_4/num.js
@@ -0,0 +1,53 @@
+let num = 0;
+for (i=0; i<11; i=i+2){
+ console.log(i)
+};
+
+
+/* инградиенты*/
+let soupWithNoodlsIng = ['potato','salt','noodls'];
+let friedEggsIng = ['eggs','onion','vagatableOil'];
+let olivieSalatIng = ['peas','eggs','mayonnaise','carrot'];
+
+ /* цены инградиентов*/
+let ingradients = {
+potato:40,
+salt:30,
+noodls: 50,
+onion:20,
+vagatableOil:100,
+peas: 90,
+eggs:100,
+mayonnaise: 120,
+carrot: 80
+};
+
+/*массив блюд*/
+let menu = [soupWithNoodlsIng,friedEggsIng,olivieSalatIng];
+let menuString = ['soupWithNoodles','friedEggsIng','olivieSalat'];
+
+/* перебираем menu и  в menu инградиенты высчитывая прибыль(прибыль 100 процентов) и по итогам заполняем объект menuProfit*/
+
+let menuProfit = {};
+count=0
+for(j in menu){
+   for( i in menu[j]){
+    ingr = menu[j][i]
+    count=count+ingradients[ingr] /* можно вставить любую прибыль*/
+
+   };
+
+   menuProfit[menuString[j]]=count
+   count=0
+};
+
+/* Вывод результата*/
+console.log(menuProfit)
+
+
+ 
+
+
+
+
+
