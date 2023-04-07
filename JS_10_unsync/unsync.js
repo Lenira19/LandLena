18d707b2f5806d21275ff2fba6869f26aa791fab
LandLena2 <e.badmazhapova@mail.ru>
2023-04-07 23:41:42 +0300

    correct

--- JS_10_unsync/unsync.js
+++ JS_10_unsync/unsync.js
@@ -9,64 +9,83 @@ fetch("menu.json")
     .then((file) => file.json())
     .then((file) => {
         ingradients = file;
-        
+
         //1 .расчет и добавление в объект себестоимости блюд
-console.log(' 1 .расчет и добавление в объект себестоимости блюд')
-menu.forEach(function (item, index, arr) {
-    let costPre = menu[index].ingradients.reduce(function (sum, item, index) {
-        return sum + ingradients[item].price;
-    }, 0);
-    menu[index].costPre = costPre; // добавляем в объект себестоимость
-});
-    console.log(menu)
+        console.log(' 1 .расчет и добавление в объект себестоимости блюд')
+        menu.forEach(function (item, index, arr) {
+            let costPre = menu[index].ingradients.reduce(function (sum, item, index) {
+                return sum + ingradients[item].price;
+            }, 0);
+            menu[index].costPre = costPre; // добавляем в объект себестоимость
+        });
+        console.log(menu)
 
 
- //2. Возвращаем массив с названием блюда и стоимостью
-console.log('2. Возвращаем массив с названием блюда и стоимостью')
+        //2. Возвращаем массив с названием блюда и стоимостью
+        console.log('2. Возвращаем массив с названием блюда и стоимостью')
 
-let delishCost = menu.map(function (item, index, arr) {
+        let delishCost = menu.map(function (item, index, arr) {
 
-    return [menu[index].name, menu[index].costPre]; F
+            return [menu[index].name, menu[index].costPre]; F
 
-});
-console.log(delishCost);
-
-
-//3. одно вегетарианское блюдо, используя `some`.
-console.log('3. одно вегетарианское блюдо, используя `some`.')
-
-let vegan = menu.some(function (item, index, arr) {
-
-   let massBuel = item.ingradients.some(function (item, index, arr) {
-   
-        
-        return ingradients[item].nonVegan == true;
-    });
-
-    
-    return !massBuel;
-});
-
-console.log(vegan);
+        });
+        console.log(delishCost);
 
 
-//4.
-console.log('4 Определите, полностью ли у вас вегетарианское меню с помощью `every`.')
-let veganEvery = menu.every(function (item, index, arr) {
+        //3. одно вегетарианское блюдо, используя `some`.
+        console.log('3. одно вегетарианское блюдо, используя `some`.')
 
-    massBuel = item.ingradients.some(function (item, index, arr) {
+        let vegan = menu.some(function (item, index, arr) {
+
+            let massBuel = item.ingradients.some(function (item, index, arr) {
+
+
+                return ingradients[item].nonVegan == true;
+            });
+
+
+            return !massBuel;
+        });
+
+        console.log(vegan);
+
+
+        //4.
+        console.log('4 Определите, полностью ли у вас вегетарианское меню с помощью `every`.')
+        let veganEvery = menu.every(function (item, index, arr) {
+
+            massBuel = item.ingradients.some(function (item, index, arr) {
                 return ingradients[item].nonVegan == true;
 
-    });
+            });
 
-    
 
-    return !massBuel;
 
-});
-console.log(veganEvery);
+            return !massBuel;
 
-    
+        });
+        console.log(veganEvery);
+
+
+        //5.
+        console.log('5 Создайте массив с вегетарианскими блюдами с помощью filter.')
+
+        let massVegan = menu.filter(function (item, index, arr) {
+
+            massBuel = item.ingradients.some(function (item, index, arr) {
+                return  ingradients[item].nonVegan == true;
+
+            });
+
+            return !massBuel
+
+        });
+
+        console.log(massVegan);
+
+
+
+
     });
 
 
